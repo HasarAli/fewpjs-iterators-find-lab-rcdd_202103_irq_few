6a518210502320ec74c79686dd2525cdc4dc7919
Hasar Ali <hasar.ali@protonmail.com>
2021-05-23 22:58:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-const record = [
-  {year: "2018", result: "L"},
-  {year: "2017", result: "W"},
-  {year: "2016", result: "N/A"},
-]
-
 function superbowlWin(record) {
   const res = record.find(game => game.result === "W")
   return res ? res.year : res
 }
-
-console.log(superbowlWin(record))
